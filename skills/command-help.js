0518fe874b9a8d147f9c451bbdb71f573c4027a3
Rodrigo <rsilvan@kaizennetwork.es>
2018-03-01 14:33:19 +0100

    Añadida la funcion de hello world

--- skills/command-help.js
+++ skills/command-help.js
@@ -7,6 +7,7 @@ module.exports = function (controller) {
         var text = "Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
+        text += "\n- " + bot.enrichCommand(message, "hello") + ": I will say hello back to you";
         bot.reply(message, text);
     });
 }
