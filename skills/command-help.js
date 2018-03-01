388a3115f5f43de91df1ffa7b72aeef70c9e41b4
Rodrigo <rsilvan@kaizennetwork.es>
2018-03-01 16:08:00 +0100

    Añadida la funcion de creacion de sala

--- skills/command-help.js
+++ skills/command-help.js
@@ -9,7 +9,7 @@ module.exports = function (controller) {
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "hello") + ": I will say hello back to you";
         text += "\n- " + bot.enrichCommand(message, "cloneroom") + ": Create a new room with same participants";
-        text += "\n- " + bot.enrichCommand(message, "add + users email separated with comes") + ": Create a new room with same participants";
+        text += "\n- " + bot.enrichCommand(message, "add + users email separated with comes") + ": Add people to this room";
         bot.reply(message, text);
     });
 }
